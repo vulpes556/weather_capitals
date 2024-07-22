6522aef52e38ade015bbca159b950b9ba29e8669
gb4rt0s <166979449+gb4rt0s@users.noreply.github.com>
2024-07-22 16:54:11 +0200

    Co-authored-by: mkalecsar <mkalecsar@users.noreply.github.com> Co-authored-by: dnkld <dnkld@users.noreply.github.com> Co-authored-by: vulpes556 <vulpes556@users.noreply.github.com>

--- frontend/script.js
+++ frontend/script.js
@@ -1,62 +1,130 @@
 // Add your Javascript code here
 //#capital-city
-let allCapitals;
+const CAPITAL_SELECTOR = document.querySelector("#capital-selector"); 
 
-
-async function fetchAllCapitals(url){
-    const response = await fetch(url)
-    return await response.json()
-
+async function fetchAllCapitals(url) {
+    const response = await fetch(url);
+    return await response.json();
 }
-const showCountryList = capitalList =>{
-    document.querySelector("#capital-selector").innerHTML = capitalList.reduce((acc,capital) =>{
-        capital.capitals.forEach(city => {
-            acc.push(`<option>${city}[${capital.name.common}]</option>`)
-        });
-        return acc
-    },[]).sort((currentCapital, nextCapital) => currentCapital.localeCompare(nextCapital)).join("")
-} 
-const mainFunction = async () =>{
-    const allCapitals = await fetchAllCapitals('https://jvvkjy8utk.execute-api.eu-central-1.amazonaws.com/tourist/api/countries/all')
-    showCountryList(allCapitals)
+const showCountryList = (capitalList) => {
+    
+    CAPITAL_SELECTOR.innerHTML += capitalList
+        .reduce((acc, capital) => {
+            capital.capitals.forEach((city) => {
+                acc.push(`<option>${city}[${capital.name.common}]</option>`);
+            });
+            return acc;
+        }, [])
+        .sort((currentCapital, nextCapital) =>
+            currentCapital.localeCompare(nextCapital)
+        )
+        .join("");
+};
+
+const mainFunction = async () => {
+    const allCapitals = await fetchAllCapitals(
+        "https://jvvkjy8utk.execute-api.eu-central-1.amazonaws.com/tourist/api/countries/all"
+    );
+    showCountryList(allCapitals);
     console.log(allCapitals);
+};
+
+mainFunction();
+
+async function fetchCountryDetails(countryName) {
+    const name = countryName
+        .split(" ")
+        .reduce(
+            (acc, str, index) =>
+                (acc +=
+                    index === countryName.split(" ").length - 1
+                        ? `name=${str.toLowerCase()}`
+                        : `name=${str.toLowerCase()}&`),
+            ""
+        );
+
+    const countryUrl = `https://jvvkjy8utk.execute-api.eu-central-1.amazonaws.com/tourist/api/countries/?${name}`;
+    const selectedCountry = await fetchAllCapitals(countryUrl);
+    document.getElementById("border-list").innerHTML = "";
+    selectedCountry.items[0].borders.forEach(async (borderingCountryCCA3) => {
+        const country = await fetchAllCapitals(
+            `https://jvvkjy8utk.execute-api.eu-central-1.amazonaws.com/tourist/api/countries/by-cca3/${borderingCountryCCA3}`
+        );
+        const liElement = document.createElement("li");
+        liElement.textContent = country.name.common;
+        document.getElementById("border-list").appendChild(liElement);
+    });
 }
 
-mainFunction()
-
-
-
-
-
-
-// https://api.weatherapi.com/v1/current.json?key=6ce7a0152aa640eb91b144442242107&q=${cityName}
-
-
-
-async function fetchWeatherDetails(cityName){
-fetch(`https://api.weatherapi.com/v1/current.json?key=6ce7a0152aa640eb91b144442242107&q=${cityName}`)
-    .then(response => response.json())
-    .then(data => {
-        display(data)
-    })
+async function fetchWeatherDetails(cityName) {
+    fetch(
+        `https://api.weatherapi.com/v1/current.json?key=6ce7a0152aa640eb91b144442242107&q=${cityName}`
+    )
+        .then((response) => response.json())
+        .then((data) => {
+            display(data);
+        });
 }
 
-
-function display(data){
-    const weatherDetails = document.getElementById("weather-details")
+function display(data) {
+    const weatherDetails = document.getElementById("weather-details");
     weatherDetails.innerHTML = `<h2>City's Name: ${data.location.name}</h2> 
-    <p>Temperature:${data.current.temp_c} </p>
+    <p>Temperature: ${data.current.temp_c} °C</p>
     <p>Weather Condition: ${data.current.condition.text}
     <img src="${data.current.condition.icon}" alt="Current weather picture.">
     </p>
-    <p>Wind: ${data.current.wind_kph} km/h Direction: ${data.current.wind_dir}</p>`
+    <p>Wind: ${data.current.wind_kph} km/h Direction: ${data.current.wind_dir}</p>
+    `;
 }
 
-window.addEventListener("input", event =>{
+window.addEventListener("input", (event) => {
     if (event.target.id === "capital-selector") {
-        fetchWeatherDetails(event.target.value.slice(0,event.target.value.indexOf("[")))
+        fetchWeatherDetails(
+            event.target.value.slice(0, event.target.value.indexOf("["))
+        );
+        fetchCountryDetails(
+            event.target.value.slice(
+                event.target.value.indexOf("[") + 1,
+                event.target.value.indexOf("]")
+            )
+        );
     }
-})
-window.addEventListener("click", event => {
-    
-})
+});
+const capitalChange = (selector) => {
+    const optionIndex =
+        CAPITAL_SELECTOR.selectedIndex;
+    const selectorElement = CAPITAL_SELECTOR;
+    const btnElement = document.querySelector(`#${selector}`);
+    if (
+        (optionIndex === 0 && selector === "prev-btn") ||
+        (optionIndex === selectorElement.length - 1 && selector === "next-btn")
+    ) {
+        btnElement.disabled = true;
+    } else {
+        btnElement.disabled = false;
+        const changedIndex =
+            selector === "prev-btn" ? optionIndex - 1 : optionIndex + 1;
+        fetchWeatherDetails(
+            CAPITAL_SELECTOR[changedIndex].value
+        );
+        CAPITAL_SELECTOR.value =
+            CAPITAL_SELECTOR[changedIndex].value;
+    }
+};
+
+window.addEventListener("click", (event) => {
+    if (event.target.id === "prev-btn" || event.target.id === "next-btn") {
+        capitalChange(event.target.id);
+    }
+    if (event.target.nodeName === "LI") {
+        fetchWeatherDetails(event.target.textContent);
+        CAPITAL_SELECTOR.value =
+            CAPITAL_SELECTOR[
+                Array.from(CAPITAL_SELECTOR).find(
+                    (element) =>
+                        element.value.includes(`[${event.target.textContent}]`)
+                ).index
+            ].value;
+            fetchCountryDetails(event.target.textContent)
+    }
+});
